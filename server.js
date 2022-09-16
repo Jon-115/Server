5f7d10b388dfece53d0f9ac962cd82c75cc29626
Jonathan Batalla <batallajjet@outlook.com>
2022-09-16 11:58:48 -0500

    added /

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var cors_proxy = require('cors-anywhere');
 
 // Listen on a specific host via the HOST environment variable
-var host = 'https://tiny-platypus-800754.netlify.app';
+var host = 'https://tiny-platypus-800754.netlify.app' + '/';
 // Listen on a specific port via the PORT environment variable
 // var port = 8080;
 
@@ -11,6 +11,6 @@ cors_proxy.createServer({
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2'],
     setHeader:{'Access-Control-Allow-Origin':'*'},
-}).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
+}).listen(host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':');
 });
