ecaf37846939615f63490da30acedbadfbaa3862
Jonathan Batalla <batallajjet@outlook.com>
2022-10-15 01:12:00 -0500

    added index.html

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var cors_proxy = require('cors-anywhere');
 
 // Listen on a specific host via the HOST environment variable
-var host = 'https://tiny-platypus-800754.netlify.app' + '/';
+var host = 'https://djdogserver2.herokuapp.com/';
 // Listen on a specific port via the PORT environment variable
 // var port = 8080;
 
