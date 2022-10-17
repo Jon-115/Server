7027b14c8e07d4e8d873f9959dfe32414ac99205
Jonathan Batalla <batallajjet@outlook.com>
2022-10-17 16:14:37 -0500

    changed host

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var cors_proxy = require('cors-anywhere');
 
 // Listen on a specific host via the HOST environment variable
-var host = 'djdogserver2.herokuapp.com/';
+var host = '127.0.0.1';
 // Listen on a specific port via the PORT environment variable
 var port = 8080;
 
